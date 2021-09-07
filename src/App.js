a04ca74101855216ae176659fffcd3aba799650a
Pinki-851 <Pinkisaroj851@outlook.com>
2021-09-07 15:07:30 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
 import React from "react";
 import "./App.css";
-import { Provider } from "react-redux";
-import Store from "./store";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import AboutScreen from "./screens/AboutScreen/AboutScreen.js";
 import HomeScreen from "./screens/HomeScreen/HomeScreen.js";
-import { Listener } from "./Listener";
 const App = () => {
   return (
-    <Provider store={Store}>
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/" component={HomeScreen} />
-          <Route exact path="/about" component={AboutScreen} />
-        </Switch>
-      </BrowserRouter>
-    </Provider>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={HomeScreen} />
+        <Route exact path="/about" component={AboutScreen} />
+      </Switch>
+    </Router>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 import React from "react";
 import reactDom from "react-dom";
+import { Provider } from "react-redux";
+import Store from "./store";
+
 import App from "./App";
 
-reactDom.render(<App />, document.getElementById("root"));
+reactDom.render(
+  <Provider store={Store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
